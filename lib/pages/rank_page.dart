5ed6e276bdf6e3667f01c3c61270d8fa1af85f2d
bonan <bonan.cao0903@gmail.com>
2020-03-27 18:03:12 +0800

    feat-排行榜页面添加吸附效果，待完善

--- lib/pages/rank_page.dart
+++ lib/pages/rank_page.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:luckyfruit/models/index.dart';
@@ -11,6 +13,8 @@ import 'package:luckyfruit/theme/public/primary_btn.dart';
 import 'package:provider/provider.dart';
 import 'package:luckyfruit/service/index.dart';
 import 'dart:convert';
+
+import 'package:visibility_detector/visibility_detector.dart';
 
 class RankPage extends StatefulWidget {
   @override
@@ -228,19 +232,58 @@ class RankPageState extends State<RankPage>
             }
         ]}
         """;
+  // TODO 走接口
+  int positionSelf = 10;
 
-  int positionSelf = 3;
+  bool showTopShield = false;
+  bool showBottomShield = true;
+
+  ScrollController _scrollController;
+
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: tabs.length, vsync: this);
-
+    _tabController.addListener(_tabBarListener);
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
     getRankListInfoData().then((res) {
       setState(() {
         friendsList = res.friends;
         bounsTreeList = res.bounsTrees;
       });
     });
+  }
+
+  _tabBarListener() {
+    setState(() {
+      if (_tabController.index == 1) {
+        showTopShield = false;
+        showBottomShield = false;
+      } else {
+        showTopShield = false;
+        showBottomShield = true;
+      }
+    });
+  }
+
+  _scrollListener() {
+    // print(
+    //     "offset=${_scrollController.offset}, maxSE=${_scrollController.position.maxScrollExtent}, minSE=${_scrollController.position.minScrollExtent}");
+    if (_scrollController.offset >=
+            _scrollController.position.maxScrollExtent &&
+        !_scrollController.position.outOfRange) {
+      // setState(() {
+      //   message = "reach the bottom";
+      // });
+    }
+    if (_scrollController.offset <=
+            _scrollController.position.minScrollExtent &&
+        !_scrollController.position.outOfRange) {
+      // setState(() {
+      //   message = "reach the top";
+      // });
+    }
   }
 
   Future<Ranklist> getRankListInfoData() async {
@@ -257,9 +300,15 @@ class RankPageState extends State<RankPage>
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+    _scrollController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     bool enableToShow = true;
-
     if (friendsList == null ||
         friendsList.isEmpty ||
         bounsTreeList == null ||
@@ -331,142 +380,227 @@ class RankPageState extends State<RankPage>
           ),
         ),
       ),
-      body: Stack(
+      body: Column(
         children: [
-          TabBarView(
-            controller: _tabController,
-            children: !enableToShow
-                ? List<Widget>.from([
-                    Center(
-                      child: Text("Loading..."),
-                    ),
-                    Center(
-                      child: Text("Loading..."),
-                    )
-                  ])
-                : List<Widget>.from([
-                    ListView.separated(
-                        itemCount: friendsList?.length,
-                        itemBuilder: (context, index) {
-                          var winnerImgSrc;
-                          var winnerWidget;
-                          if (index == 0) {
-                            winnerImgSrc =
-                                "assets/image/rank_page_winner_first.png";
-                          } else if (index == 1) {
-                            winnerImgSrc =
-                                "assets/image/rank_page_winner_second.png";
-                          } else if (index == 2) {
-                            winnerImgSrc =
-                                "assets/image/rank_page_winner_third.png";
-                          }
+          Expanded(
+            child: Container(
+              width: ScreenUtil().setWidth(1080),
+              child: TabBarView(
+                controller: _tabController,
+                children: !enableToShow
+                    ? List<Widget>.from([
+                        Center(
+                          child: Text("Loading..."),
+                        ),
+                        Center(
+                          child: Text("Loading..."),
+                        )
+                      ])
+                    : List<Widget>.from([
+                        Stack(
+                          children: [
+                            NotificationListener<ScrollNotification>(
+                              onNotification: (scrollNotification) {
+                                if (scrollNotification
+                                    is ScrollStartNotification) {
+                                  print(
+                                      "notifyStart ${scrollNotification.metrics}");
+                                } else if (scrollNotification
+                                    is ScrollUpdateNotification) {
+                                  print(
+                                      "notifyUpdate ${scrollNotification.metrics}");
+                                  double thresholdValue = .5;
+                                  double value = positionSelf *
+                                          ScreenUtil().setWidth(200) -
+                                      scrollNotification.metrics.pixels;
+                                  if (value < thresholdValue &&
+                                      !showTopShield) {
+                                    setState(() {
+                                      showTopShield = true;
+                                    });
+                                  } else if (value > thresholdValue &&
+                                      showTopShield) {
+                                    setState(() {
+                                      showTopShield = false;
+                                    });
+                                  }
 
-                          if (index < 3) {
-                            winnerWidget = Image.asset(
-                              winnerImgSrc,
-                              width: ScreenUtil().setWidth(110),
-                              height: ScreenUtil().setWidth(120),
-                            );
-                          } else {
-                            winnerWidget = Container(
-                                alignment: Alignment.center,
-                                width: ScreenUtil().setWidth(110),
+                                  double value1 = (positionSelf + 1) *
+                                          ScreenUtil().setWidth(200) -
+                                      (scrollNotification.metrics.pixels +
+                                          scrollNotification
+                                              .metrics.extentInside);
+
+                                  if (value1 < thresholdValue &&
+                                      showBottomShield) {
+                                    setState(() {
+                                      showBottomShield = false;
+                                    });
+                                  } else if (value1 > thresholdValue &&
+                                      !showBottomShield) {
+                                    setState(() {
+                                      showBottomShield = true;
+                                    });
+                                  }
+                                } else if (scrollNotification
+                                    is ScrollEndNotification) {
+                                  print(
+                                      "notifyEnd ${scrollNotification.metrics}");
+                                }
+                              },
+                              child: ListView.separated(
+                                  controller: _scrollController,
+                                  itemCount: friendsList?.length,
+                                  itemBuilder: (context, index) =>
+                                      getListviewItem(index),
+                                  separatorBuilder:
+                                      (BuildContext context, int index) =>
+                                          const Divider(
+                                            height: 0,
+                                          )),
+                            ),
+                            positionSelf >= 0 && showTopShield
+                                ? Positioned(
+                                    child: Container(
+                                        color: Colors.red,
+                                        child: getListviewItem(positionSelf)))
+                                : Container(width: 0, height: 0),
+                            positionSelf >= 0 && showBottomShield
+                                ? Positioned(
+                                    bottom: 0,
+                                    child: Container(
+                                        width: ScreenUtil().setWidth(1080),
+                                        height: ScreenUtil().setWidth(200),
+                                        child: getListviewItem(positionSelf)))
+                                : Container(width: 0, height: 0),
+                          ],
+                        ),
+                        // ),
+
+                        // 第二个tab
+                        ListView.separated(
+                            itemCount: bounsTreeList?.length,
+                            itemBuilder: (context, index) {
+                              var avatarWidget = CompatibleNetworkAvatarWidget(
+                                bounsTreeList[index].avatar,
+                                defaultImageUrl:
+                                    "assets/image/rank_page_portrait_default.png",
+                                width: ScreenUtil().setWidth(120),
                                 height: ScreenUtil().setWidth(120),
-                                child: Text("${index + 1}"));
-                          }
+                                fit: BoxFit.cover,
+                              );
 
-                          var avatarWidget = CompatibleNetworkAvatarWidget(
-                            friendsList[index].avatar,
-                            defaultImageUrl:
-                                "assets/image/rank_page_portrait_default.png",
-                            width: ScreenUtil().setWidth(120),
-                            height: ScreenUtil().setWidth(120),
-                            fit: BoxFit.cover,
-                          );
-
-                          return Container(
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: ScreenUtil().setWidth(50)),
-                              color: ((index == positionSelf)
-                                  ? Color(0xFFBCFFCC)
-                                  : Colors.white),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  winnerWidget,
-                                  Expanded(
-                                    child: ListTile(
-                                      // leading: ClipRRect(borderRadius:BorderRadius.circular(30), child: avatarWidget),
-                                      leading: ClipOval(child: avatarWidget),
-                                      title:
-                                          Text(friendsList[index].rela_account),
-                                      //TODO 这行描述是否从接口中返回？还是前端写死的？
-                                      subtitle: Text(friendsList[index]
-                                                  .tree_num >
-                                              0
-                                          ? "${friendsList[index].tree_num} global dividend tree(s)"
-                                          : "No global dividend dog"),
-                                    ),
-                                  ),
-                                  Text(
-                                    "\$${friendsList[index].separate_amount}",
-                                    style: TextStyle(color: Colors.red),
-                                  ),
-                                ],
-                              ));
-                        },
-                        separatorBuilder: (BuildContext context, int index) =>
-                            const Divider(
-                              height: 1,
-                            )),
-
-                    // 第二个tab
-                    ListView.separated(
-                        itemCount: bounsTreeList?.length,
-                        itemBuilder: (context, index) {
-                          var avatarWidget = CompatibleNetworkAvatarWidget(
-                            bounsTreeList[index].avatar,
-                            defaultImageUrl:
-                                "assets/image/rank_page_portrait_default.png",
-                            width: ScreenUtil().setWidth(120),
-                            height: ScreenUtil().setWidth(120),
-                            fit: BoxFit.cover,
-                          );
-
-                          return Container(
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: ScreenUtil().setWidth(50),
-                                  vertical: ScreenUtil().setWidth(20)),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  ClipOval(child: avatarWidget),
-                                  Text(bounsTreeList[index].rela_account),
-                                  Text("1"),
-                                  Text(bounsTreeList[index].separate_amount,
-                                      style: TextStyle(
-                                          color:
-                                              Color.fromARGB(255, 76, 47, 1))),
-                                ],
-                              ));
-                        },
-                        separatorBuilder: (BuildContext context, int index) =>
-                            const Divider())
-                  ]),
+                              return Container(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: ScreenUtil().setWidth(50),
+                                      vertical: ScreenUtil().setWidth(20)),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: <Widget>[
+                                      ClipOval(child: avatarWidget),
+                                      Text(bounsTreeList[index].rela_account),
+                                      Text("1"),
+                                      Text(bounsTreeList[index].separate_amount,
+                                          style: TextStyle(
+                                              color: Color.fromARGB(
+                                                  255, 76, 47, 1))),
+                                    ],
+                                  ));
+                            },
+                            separatorBuilder:
+                                (BuildContext context, int index) =>
+                                    const Divider())
+                      ]),
+              ),
+            ),
           ),
-          Positioned(
-              bottom: 0,
-              child: Container(
-                color: Colors.white,
-                width: ScreenUtil().setWidth(1080),
-                height: ScreenUtil().setWidth(244),
-                alignment: Alignment(0, 0),
-                child: PrimaryButton(text: "Invite", width: 600, height: 124),
-              ))
+          Container(
+            color: Colors.white,
+            width: ScreenUtil().setWidth(1080),
+            height: ScreenUtil().setWidth(244),
+            alignment: Alignment(0, 0),
+            child: GestureDetector(
+                onTap: () {
+                  print("_tabController.index= ${_tabController.index}");
+                },
+                child: PrimaryButton(text: "Invite", width: 600, height: 124)),
+          ),
         ],
       ),
+    );
+  }
+
+  Widget getListviewItem(int index) {
+    if (index < 0 ||
+        friendsList?.length == null ||
+        index >= friendsList?.length) {
+      return Container(width: 0, height: 0);
+    }
+    // debugger(when: index == 3);
+    var winnerImgSrc;
+    var winnerWidget;
+    if (index == 0) {
+      winnerImgSrc = "assets/image/rank_page_winner_first.png";
+    } else if (index == 1) {
+      winnerImgSrc = "assets/image/rank_page_winner_second.png";
+    } else if (index == 2) {
+      winnerImgSrc = "assets/image/rank_page_winner_third.png";
+    }
+
+    if (index < 3) {
+      winnerWidget = Image.asset(
+        winnerImgSrc,
+        width: ScreenUtil().setWidth(110),
+        height: ScreenUtil().setWidth(120),
+      );
+    } else {
+      winnerWidget = Container(
+          alignment: Alignment.center,
+          width: ScreenUtil().setWidth(110),
+          height: ScreenUtil().setWidth(120),
+          child: Text("${index + 1}"));
+    }
+
+    var avatarWidget = CompatibleNetworkAvatarWidget(
+      friendsList[index]?.avatar,
+      defaultImageUrl: "assets/image/rank_page_portrait_default.png",
+      width: ScreenUtil().setWidth(120),
+      height: ScreenUtil().setWidth(120),
+      fit: BoxFit.cover,
+    );
+
+    return VisibilityDetector(
+      key: Key("unique_key_$index"),
+      onVisibilityChanged: (e) {
+        debugPrint(
+            "debugPrint-visible: ${e.key}, ${e.visibleFraction}, ${e.size}, ${e.visibleBounds}");
+      },
+      child: Container(
+          width: ScreenUtil().setWidth(1080),
+          height: ScreenUtil().setWidth(200),
+          padding: EdgeInsets.symmetric(horizontal: ScreenUtil().setWidth(50)),
+          color: ((index == positionSelf) ? Color(0xFFBCFFCC) : Colors.white),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              winnerWidget,
+              Expanded(
+                child: ListTile(
+                  leading: ClipOval(child: avatarWidget),
+                  title: Text(friendsList[index].rela_account),
+                  subtitle: Text(friendsList[index].tree_num > 0
+                      ? "${friendsList[index].tree_num} global dividend tree(s)"
+                      : "No global dividend dog"),
+                ),
+              ),
+              Text(
+                "\$${friendsList[index].separate_amount}",
+                style: TextStyle(color: Colors.red),
+              ),
+            ],
+          )),
     );
   }
 }
