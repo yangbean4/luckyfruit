9aa912565df8654c5ca0928b2b6fff94aa9470b0
bin.yang <bean_4@163.com>
2020-04-08 14:28:03 +0800

    feat(add no data page):

--- lib/pages/mine/message.dart
+++ lib/pages/mine/message.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:luckyfruit/widgets/no_data.dart';
 import 'package:provider/provider.dart';
 
 import 'package:luckyfruit/models/index.dart' show Message;
@@ -57,78 +58,84 @@ class _MessagePageState extends State<MessagePage> {
           ),
         ),
       ),
-      body: ListView.separated(
-          itemCount: msgList.length,
-          // itemExtent: ScreenUtil().setWidth(490 + 32),
-          physics: BouncingScrollPhysics(),
-          padding: EdgeInsets.symmetric(
-              horizontal: ScreenUtil().setWidth(60),
-              vertical: ScreenUtil().setWidth(60)),
-          separatorBuilder: (BuildContext context, int index) => Divider(
-                height: ScreenUtil().setWidth(32).toDouble(),
-              ),
-          itemBuilder: (context, index) {
-            Message message = msgList[index];
-            return Container(
-              width: ScreenUtil().setWidth(960),
-              height: ScreenUtil().setWidth(490),
+      body:
+          //  msgList.isEmpty
+          //     ? NoData(
+          //         msg: 'no....data...',
+          //       )
+          //     :
+          ListView.separated(
+              itemCount: msgList.length,
+              // itemExtent: ScreenUtil().setWidth(490 + 32),
+              physics: BouncingScrollPhysics(),
               padding: EdgeInsets.symmetric(
-                  horizontal: ScreenUtil().setWidth(80),
-                  vertical: ScreenUtil().setWidth(44)),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(
-                      Radius.circular(ScreenUtil().setWidth(40)))),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Image.asset(
-                    'assets/image/${message.wdl_status == '2' ? 'error' : 'success'}.png',
-                    width: ScreenUtil().setWidth(151),
-                    height: ScreenUtil().setWidth(151),
+                  horizontal: ScreenUtil().setWidth(60),
+                  vertical: ScreenUtil().setWidth(60)),
+              separatorBuilder: (BuildContext context, int index) => Divider(
+                    height: ScreenUtil().setWidth(32).toDouble(),
                   ),
-                  Text(
-                    message.wdl_status == '2'
-                        ? 'cash out success'
-                        : 'cash out failed',
-                    style: TextStyle(
-                        color: MyTheme.blackColor,
-                        fontSize: ScreenUtil().setSp(70),
-                        fontFamily: FontFamily.bold,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  message.wdl_status == '2'
-                      ? Text(
-                          "Your \$${message.wdl_amt} cash out failed,Please check if you haven't login with FB",
-                          style: TextStyle(
-                              color: MyTheme.blackColor,
-                              fontSize: ScreenUtil().setSp(34),
-                              fontFamily: FontFamily.regular,
-                              fontWeight: FontWeight.w400))
-                      : RichText(
-                          textAlign: TextAlign.center,
-                          text: TextSpan(
-                              text:
-                                  'your \$${message.wdl_amt} Amazon Gift card redeeded sucessfully,your came is:',
+              itemBuilder: (context, index) {
+                Message message = msgList[index];
+                return Container(
+                  width: ScreenUtil().setWidth(960),
+                  height: ScreenUtil().setWidth(490),
+                  padding: EdgeInsets.symmetric(
+                      horizontal: ScreenUtil().setWidth(80),
+                      vertical: ScreenUtil().setWidth(44)),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                          Radius.circular(ScreenUtil().setWidth(40)))),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Image.asset(
+                        'assets/image/${message.wdl_status == '2' ? 'error' : 'success'}.png',
+                        width: ScreenUtil().setWidth(151),
+                        height: ScreenUtil().setWidth(151),
+                      ),
+                      Text(
+                        message.wdl_status == '2'
+                            ? 'cash out success'
+                            : 'cash out failed',
+                        style: TextStyle(
+                            color: MyTheme.blackColor,
+                            fontSize: ScreenUtil().setSp(70),
+                            fontFamily: FontFamily.bold,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      message.wdl_status == '2'
+                          ? Text(
+                              "Your \$${message.wdl_amt} cash out failed,Please check if you haven't login with FB",
                               style: TextStyle(
                                   color: MyTheme.blackColor,
                                   fontSize: ScreenUtil().setSp(34),
                                   fontFamily: FontFamily.regular,
-                                  fontWeight: FontWeight.w400),
-                              children: [
-                                TextSpan(
-                                    text: message.gift_id,
-                                    style: TextStyle(
-                                        color: MyTheme.blackColor,
-                                        fontSize: ScreenUtil().setSp(34),
-                                        fontFamily: FontFamily.semibold,
-                                        fontWeight: FontWeight.w400))
-                              ]),
-                        ),
-                ],
-              ),
-            );
-          }),
+                                  fontWeight: FontWeight.w400))
+                          : RichText(
+                              textAlign: TextAlign.center,
+                              text: TextSpan(
+                                  text:
+                                      'your \$${message.wdl_amt} Amazon Gift card redeeded sucessfully,your came is:',
+                                  style: TextStyle(
+                                      color: MyTheme.blackColor,
+                                      fontSize: ScreenUtil().setSp(34),
+                                      fontFamily: FontFamily.regular,
+                                      fontWeight: FontWeight.w400),
+                                  children: [
+                                    TextSpan(
+                                        text: message.gift_id,
+                                        style: TextStyle(
+                                            color: MyTheme.blackColor,
+                                            fontSize: ScreenUtil().setSp(34),
+                                            fontFamily: FontFamily.semibold,
+                                            fontWeight: FontWeight.w400))
+                                  ]),
+                            ),
+                    ],
+                  ),
+                );
+              }),
     );
   }
 }
--- lib/widgets/no_data.dart
+++ lib/widgets/no_data.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:luckyfruit/theme/index.dart';
+
 class NoData extends StatelessWidget {
   final String msg;
   const NoData({Key key, this.msg}) : super(key: key);
@@ -7,7 +10,27 @@ class NoData extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        // child: ,
-        );
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Image.asset(
+          'assets/image/no_data.png',
+          width: ScreenUtil().setWidth(466),
+          height: ScreenUtil().setWidth(527),
+        ),
+        Container(
+          height: ScreenUtil().setWidth(90),
+          child: null,
+        ),
+        Text(
+          msg,
+          style: TextStyle(
+              color: MyTheme.tipsColor,
+              fontFamily: FontFamily.semibold,
+              fontSize: ScreenUtil().setWidth(46)),
+        )
+      ],
+    ));
   }
 }
